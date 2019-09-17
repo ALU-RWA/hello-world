693832bb8748229e6ec15873dc6d493fe4c54c3d
Nobert-Ok <o.chukwuebu@alustudent.com>
2019-09-17 14:29:15 +0200

    Assignment1

---
+++ function.py
@@ -0,0 +1,10 @@
+def greet(name):
+    print("Hello beautiful"+name)
+greet('Elle')
+
+
+def rectangle_perimeter(x,y):
+    perimeter = (x + y)*2
+    print("the perimter is", str(perimeter))
+
+rectangle_perimeter(4,6)
